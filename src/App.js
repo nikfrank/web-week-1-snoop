f8bc330206f72fe7de5e91f496d57462da01933c
nik frank <nikbfrank@gmail.com>
2019-06-25 02:23:53 -0700

    email regex

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import emailRegex from './emailRegex';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const checkEmail = (email)=> emailRegex.test(email);
+
+
+class App extends React.Component {
+
+  state = {
+    rapName: '',
+    email: '',
+    isEmailValid: false,
+  }
+
+  setRapName = (event)=> {
+    this.setState({
+      rapName: event.target.value
+    })
+  }
+
+  setEmail = (event)=> {
+    this.setState({
+      email: event.target.value,
+      isEmailValid: checkEmail(event.target.value),
+    })
+  }
+
+  done = (event)=> {
+    console.log('done applying, it\'s friday, now I got nothin to do');
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <div className='form'>
+          <div className='card swanky-input-container'>
+            <label>
+              <input value={this.state.rapName} onChange={this.setRapName} />
+              <span className='title'>Rap Name</span>
+            </label>
+          </div>
+
+          <div className='card swanky-input-container'>
+            <label>
+              <input value={this.state.email} onChange={this.setEmail} />
+              <span className='title'>Email</span>
+              {
+                (this.state.isEmailValid || this.state.email.length < 1) ? (null) : (
+                  <span className='email-invalid'>Please enter a valid email</span>
+                )
+              }
+            </label>
+          </div>
+
+          <div className='done-container'>
+            <button className='done-button' onClick={this.done}>Done</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
+
+
+
+
 
 export default App;
