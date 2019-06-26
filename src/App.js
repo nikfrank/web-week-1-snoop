e40e5d7e6adfd8c33284a1c8b1081ed7a83a7d3c
nik frank <nikbfrank@gmail.com>
2019-06-26 01:01:42 -0700

    svg x button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import emailRegex from './emailRegex';
+import goldRecord from './goldRecord.png';
 
 const checkEmail = (email)=> emailRegex.test(email);
 
@@ -8,14 +9,16 @@ const checkEmail = (email)=> emailRegex.test(email);
 class App extends React.Component {
 
   state = {
+    modalOpen: false,
     rapName: '',
     email: '',
     isEmailValid: false,
+    albumSales: 1000,
   }
 
   setRapName = (event)=> {
     this.setState({
-      rapName: event.target.value
+      rapName: event.target.value,
     })
   }
 
@@ -26,7 +29,19 @@ class App extends React.Component {
     })
   }
 
+  setAlbumSales = (event) => {
+    this.setState({
+      albumSales: Math.max(0, Number(event.target.value)),
+    })
+  }
+
+  toggleModal = ()=>
+    this.setState({
+      modalOpen: !this.state.modalOpen
+    })
+
   done = (event)=> {
+    this.toggleModal();
     console.log('done applying, it\'s friday, now I got nothin to do');
   }
 
@@ -53,9 +68,40 @@ class App extends React.Component {
             </label>
           </div>
 
-          <div className='done-container'>
-            <button className='done-button' onClick={this.done}>Done</button>
+          <div className='card swanky-input-container'>
+            <label>
+              <input type='number' step={100000}
+                     value={this.state.albumSales} onChange={this.setAlbumSales} />
+              <span className='title'>Album Sales</span>
+            </label>
+            <div className='goldRecords'>
+            {
+              [1,2,3,4,5,6]
+              .filter(x => x*1000000 < this.state.albumSales)
+              .map(n=> (
+                <div className='goldRecord' key={n}>
+                  <img src={goldRecord}/>
+                </div>
+              ))
+            }
+            </div>
           </div>
+
+          <div className='done-container'>
+            <button className='done-button' onClick={this.toggleModal}>Done</button>
+          </div>
+        </div>
+
+        <div className={this.state.modalOpen ? 'modal-open' : 'modal-closed'}>
+          <h2>Confirmation</h2>
+          <p>are you sure you are ready with your application?</p>
+          <button onClick={this.done}>Confirm!</button>
+          <svg viewBox='0 0 100 100' className='x-button'
+               onClick={this.toggleModal}>
+            <circle cx={50} cy={50} r={47}/>
+            <path d='M 30 30 L 70 70'/>
+            <path d='M 70 30 L 30 70'/>
+          </svg>
         </div>
       </div>
     );
